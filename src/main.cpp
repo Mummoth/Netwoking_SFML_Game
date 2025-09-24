e9abaa6b61ade6e215cbcb4674c9d21462a8bc6a
Piotr Piwoni <piotrpiwonibiz@gmail.com>
2025-09-24 15:20:11 +0100

    Starting rendering a small sprite on screen. . Converted the render loop into an operate render thread.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
+#include <functional>
 #include <iostream>
-#include <SFML/Window.hpp>
+#include <thread>
+#include <SFML/Graphics.hpp>
+
+#define TXT_PATH L"assets/textures/"
+
+
+void RenderingThread(sf::RenderWindow& window, const std::vector<sf::Drawable*>& drawables);
 
 int main()
 {
-	sf::Window window(sf::VideoMode({ 800,600 }), "My Window");
+	auto window = std::make_unique<sf::RenderWindow>(sf::VideoMode({ 800, 600 }), "My Window");
+	// Deactivate context in main thread.
+	static_cast<void>(window->setActive(false)); //< See casting reason in RenderingThread().
 
-	while (window.isOpen())
+	sf::Texture txt(TXT_PATH "mam.png");
+	sf::Sprite sprite(txt);
+
+	std::vector<sf::Drawable*> drawables;
+	drawables.reserve(20);
+	drawables.push_back(&sprite);
+
+	std::thread renderThread(&RenderingThread, std::ref(*window), std::ref(drawables));
+
+	sprite.scale({0.5f,0.5f});
+
+
+	while (window->isOpen())
 	{
-		while (const std::optional event = window.pollEvent())
+		while (const std::optional event = window->pollEvent())
 		{
 			if (event->is<sf::Event::Closed>())
-				window.close();
+				window->close();
 
 			// Handle keyboard input.
 			else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
@@ -18,13 +39,31 @@ int main()
 				switch (keyPressed->scancode)
 				{
 				case sf::Keyboard::Scan::Escape:
-					window.close();
+					window->close();
 				}
 			}
 		}
 	}
 
+	renderThread.join();
 	std::cout << "Hello World\n";
 
 	return 0;
+}
+
+void RenderingThread(sf::RenderWindow& window, const std::vector<sf::Drawable*>& drawables)
+{
+	// Activate the window's context.
+	static_cast<void>(window.setActive(true));//< Cast to VOID to ignore discard warning.
+
+	while (window.isOpen())
+	{
+		window.clear();
+
+		// Draw here.
+		for (const auto obj : drawables)
+			window.draw(*obj);
+
+		window.display();
+	}
 }
